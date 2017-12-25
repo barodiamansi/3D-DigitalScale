a2dc3e676c3614f2581fa17c3d4eee44bc507cdb
Mansi Barodia <mansibarodia@MacBook-Pro.local>
2017-12-25 17:18:36 -0600

    Create a digital scale using 3d touch capability.

---
+++ DigitalScale/DigitalScale/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DigitalScale
+//
+//  Created by Mansi Barodia on 12/24/17.
+//  Copyright © 2017 Mansi Barodia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
